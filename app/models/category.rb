96cccf3c44dd72b16e6a527c54ca01218503a563
s-i27 <s_d27@icloud.com>
2020-11-12 17:48:29 +0900

    ID修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,14 +1,14 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '経済' },
-    { id: 3, name: '政治' },
-    { id: 4, name: '地域' },
-    { id: 5, name: '国際' },
-    { id: 6, name: 'IT' },
-    { id: 7, name: 'エンタメ' },
-    { id: 8, name: 'スポーツ' },
-    { id: 9, name: 'グルメ' },
+    { id: 2, name: 'サッカー、フットサル' },
+    { id: 3, name: 'ボルタリング' },
+    { id: 4, name: '野球' },
+    { id: 5, name: 'ダーツ' },
+    { id: 6, name: 'バレーボール' },
+    { id: 7, name: 'バトミントン' },
+    { id: 8, name: 'テニス' },
+    { id: 9, name: '卓球' },
     { id: 10, name: 'その他' }
     ]
 end
